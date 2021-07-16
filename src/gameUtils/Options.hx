82459e13e7624cdad8c209f7be11f166e9e3d1d8
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2021-07-16 15:40:05 +0100

    Options class

---
+++ src/gameUtils/Options.hx
@@ -0,0 +1,90 @@
+package gameUtils;
+
+class Options {
+
+	var savePath:String;
+	var data:Dynamic;
+	var defaults:Dynamic;
+
+	public function new(appName:String) {
+		defaults = generateDefaults();
+
+		savePath = "data";
+
+		#if sys
+		try {
+			var xdgConfig:String = Sys.getEnv("XDG_CONFIG_HOME");
+			if (xdgConfig == null) {
+				xdgConfig = haxe.io.Path.join([Sys.getEnv("HOME"), ".config"]);
+			}
+			var saveDir:String = haxe.io.Path.join([xdgConfig, appName]);
+			if (!sys.FileSystem.exists(saveDir)) {
+				sys.FileSystem.createDirectory(saveDir);
+			}
+			savePath = haxe.io.Path.join([saveDir, savePath]);
+		}
+		catch (e) {
+			Sys.stderr().writeString("Couldn't create config directory, saving data to current working directory instead");
+		}
+		#end
+	}
+
+	public function load() {
+		var serialised:String;
+		#if sys
+		try {
+			serialised = sys.io.File.getContent(savePath);
+		}
+		catch (e) {
+			Sys.stderr().writeString("Couldn't find config dir");
+			serialised = "{}";
+		}
+		#elseif js
+		serialised = js.Browser.window.localStorage.getItem(savePath);
+		#else
+		throw "Not implemented";
+		#end
+		data = haxe.Json.parse(serialised);
+	}
+
+	public function save() {
+		if (data == null) {
+			data = {};
+		}
+		var serialised:String = haxe.Json.stringify(data);
+		#if sys
+		sys.io.File.saveContent(savePath, serialised);
+		#elseif js
+		js.Browser.window.localStorage.setItem(savePath, serialised);
+		#else
+		throw "Not implemented";
+		#end
+		load();
+	}
+
+	public function get(key:String):Dynamic {
+		if (data == null) {
+			load();
+		}
+		var result:Dynamic = Reflect.field(data, key);
+		if (result == null) {
+			return Reflect.field(defaults, key);
+		}
+		else {
+			return result;
+		}
+	}
+
+	public function set(key:String, value) {
+		if (data == null) {
+			data = {};
+		}
+		Reflect.setField(data, key, value);
+	}
+
+	/** Override this in a subclass to generate defaults values **/
+	function generateDefaults():Dynamic {
+		return {};
+	}
+
+}
